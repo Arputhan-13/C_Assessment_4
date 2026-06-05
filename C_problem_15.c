9de33a983b168ab5be00f22aa2fd179070c12f81
Arputhan-13 <karputhan@gmail.com>
2026-06-05 22:09:50 +0530

    Create C_problem_15.c

---
+++ C_problem_15.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c, o, m, y=0,f,sum, h, k=1,n=1;
+    printf("Enter the Number : ");
+    scanf("%d",&a);
+    o=a/10;
+    c=a%10;
+    start:
+    if(a>0)
+    {
+    b=a%10;
+    sum=b;
+    a=a/10;
+    }
+    if(a!=0)
+    goto start;
+    m=o;
+    end:
+    if(m>0)
+    {
+    m=m/10;
+    y=y+1;
+    }
+    if(m!=0)
+    goto end;
+    centre:
+    if(k<y)
+    {
+    n=n*10;
+    k++;
+    goto centre;
+    }
+    f=o%n;
+    
+    printf("%d%d%d",sum-1,f,c);
+}
