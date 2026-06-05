51db7fddf3d09e36eaee0b56d28ae36ae0480087
Arputhan-13 <karputhan@gmail.com>
2026-06-05 22:04:59 +0530

    Create C_Problem_7.c

---
+++ C_Problem_7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a=10,b, c, d;
+    start:
+    if(a%2==1)
+    {
+       b=a%10;
+       c=(a/10)%10;
+       d=(b+c==7)*a;
+       if(d!=0)
+       {
+        printf("%d\n",d);
+        }
+    }
+    a++;
+    if(a<100)
+    goto start;
+}
